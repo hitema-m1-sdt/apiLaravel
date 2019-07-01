b7384a5210be246b34a69143e6c1602e275fc585
NicoSar <nicolas.sarrazin88@gmail.com>
2019-07-01 14:39:01 +0200

    lesson controller test get all and get unique

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/getAllLesson', 'LessonController@index')->name('getAllLesson');
+Route::get('/getLesson', 'LessonController@index')->name('getLesson');
