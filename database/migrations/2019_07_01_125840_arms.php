82ede3083960f25a0251f5dac5937727ce90b1e0
NicoSar <nicolas.sarrazin88@gmail.com>
2019-07-02 11:49:25 +0200

    seeder + add armcontroller + edit migration + route api

--- database/migrations/2019_07_01_125840_arms.php
+++ database/migrations/2019_07_01_125840_arms.php
@@ -14,8 +14,8 @@ class Arms extends Migration
     public function up()
     {
         schema::create('arms', function (Blueprint $table){
-        $table->bigIncrements('id');
-        $table->string('name');
+          $table->bigIncrements('id');
+          $table->string('name');
         });
     }
 
